ee414979eda4d1ae28827060390fb3ea4315dd27
Orange <orangegamerz2124@gmail.com>
2024-04-02 23:09:19 +0530

    first run

--- medical/app/api/patient/route.ts
+++ medical/app/api/patient/route.ts
@@ -25,3 +25,20 @@ export async function POST(req:Request) {
     }
     
 }
+export async function GET(req: Request) {
+    try {
+        const profile = await currentProfile();
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        const patients = await db.patient.findMany({
+            where:{
+                profileId:profile.id
+            }
+        });
+        return NextResponse.json(patients);
+    } catch (error) {
+        console.error("[PATIENT_GET]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}

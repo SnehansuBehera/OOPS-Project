d4f25b21c9ac789d75ed5ab8336fc7788a58a0dc
Orange <orangegamerz2124@gmail.com>
2024-04-04 19:20:26 +0530

    change in patient model

--- medical/app/test2/page.tsx
+++ medical/app/test2/page.tsx
@@ -7,9 +7,14 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { FileUpload } from "@/components/file-upload";
 const formSchema=z.object({
     name: z.string().min(1, { message: "Patient name required" }),
     age: z.number().int().positive({ message: "Age must be a positive integer" }),
+    height: z.number().int().positive({ message: "Height must be a positive integer" }),
+    weight: z.number().int().positive({ message: "Weight must be a positive integer" }),
+    imageUrl:z.string(),
+    bloodgroup:z.string(),
     sex: z.string(),
 })
  const Patient = () => {
@@ -20,7 +25,11 @@ const formSchema=z.object({
             defaultValues: {
                 name: "",
                 age: 0,
-                sex:"",
+                height:0,
+                weight:0,
+                imageUrl:"",
+                bloodgroup:"A+",
+                sex:"MALE",
 
             }
         }
@@ -47,6 +56,27 @@ const formSchema=z.object({
         <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8  px-6">
+            <div className="flex items-center justify-center">
+                            <FormField
+                                control={form.control}
+                                name="imageUrl"
+                                render={({field})=>(
+                                    <FormItem>
+                                        <FormLabel className="uppercase text-l font-bold text-zinc-500 dark:text-secondary/70">
+                                             Patient Image
+                                        </FormLabel>
+                                        <FormControl>
+                                            <FileUpload
+                                                endpoint="memberImage"
+                                                value={field.value}
+                                                onChange={field.onChange}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}>
+                                </FormField>
+                        </div>
                 <FormField
                     control={form.control}
                     name="name"
@@ -80,7 +110,57 @@ const formSchema=z.object({
                                     type="number"
                                     disabled={isLoading}
                                     className="bg-zinc-300/50 text-black"
-                                    placeholder="Enter your age"
+                                    placeholder="Enter patient's age"
+                                    {...field}
+                                    onChange={(e) => {
+                                        const value = parseInt(e.target.value);
+                                        field.onChange(value);
+                                    }}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="height"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="uppercase text-l font-bold text-zinc-500 dark:text-secondary/70">
+                                Height in centimeters
+                            </FormLabel>
+                            <FormControl>
+                                <Input
+                                    type="number"
+                                    disabled={isLoading}
+                                    className="bg-zinc-300/50 text-black"
+                                    placeholder="Enter patient's height"
+                                    {...field}
+                                    onChange={(e) => {
+                                        const value = parseInt(e.target.value);
+                                        field.onChange(value);
+                                    }}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="weight"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="uppercase text-l font-bold text-zinc-500 dark:text-secondary/70">
+                                Weight in kilograms
+                            </FormLabel>
+                            <FormControl>
+                                <Input
+                                    type="number"
+                                    disabled={isLoading}
+                                    className="bg-zinc-300/50 text-black"
+                                    placeholder="Enter patient's weight"
                                     {...field}
                                     onChange={(e) => {
                                         const value = parseInt(e.target.value);
@@ -104,6 +184,30 @@ const formSchema=z.object({
                                 <select {...field} disabled={isLoading} className="bg-zinc-300/50 text-black" onChange={(e) => field.onChange(e.target.value)}>
                                     <option value={"MALE"}>Male</option>
                                     <option value={"FEMALE"}>Female</option>
+                                </select>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                 <FormField
+                    control={form.control}
+                    name="bloodgroup"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="uppercase pr-4 text-l font-bold text-zinc-500 dark:text-secondary/70">
+                                Blood Group
+                            </FormLabel>
+                            <FormControl>
+                                <select {...field} disabled={isLoading} className="bg-zinc-300/50 text-black" onChange={(e) => field.onChange(e.target.value)}>
+                                    <option value={"A+"}>A+</option>
+                                    <option value={"A-"}>A-</option>
+                                    <option value={"B+"}>B+</option>
+                                    <option value={"B-"}>B-</option>
+                                    <option value={"AB+"}>AB+</option>
+                                    <option value={"AB-"}>AB-</option>
+                                    <option value={"O+"}>O+</option>
+                                    <option value={"O-"}>O-</option>
                                 </select>
                             </FormControl>
                             <FormMessage />
